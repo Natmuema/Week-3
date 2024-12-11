b7e7820367bda459236cc575768d3ad645de54e4
Natmuema <nkalondumuema@gmail.com>
2024-12-11 20:18:49 +0300

    week 3 react project

---
+++ todo-list-app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Todo from './components/Todo'
+
+
+const App = () => {
+  return (
+    <>
+    <div className='bg-sky-200 grid py-4 min-h-screen'>
+      <Todo />
+    </div>
+    </>
+  )
+}
+
+export default App
